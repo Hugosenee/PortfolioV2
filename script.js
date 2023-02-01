63567f929545fa310db2ebfa36a104f13d6ec43a
Hugo Senée <113121085+Hugosenee@users.noreply.github.com>
2023-02-01 22:39:41 +0100

    :sparkles: few littles add

--- script.js
+++ script.js
@@ -171,3 +171,26 @@ const closeMail = () => {
     displaymail.classList.remove('showMail')
     displaymail.classList.add('removeMail')
 }
+
+// apparaitre div scroll
+
+const timing = .7
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: timing
+}
+
+const HandleIntersect = function (entries , observer) {
+    entries.forEach((entry) => {
+        if(entry.intersectionRatio > timing){
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+    })
+}
+
+const observer = new IntersectionObserver(HandleIntersect, options);
+document.querySelectorAll('.reveal').forEach(function (reveal) {
+    observer.observe(reveal)
+});
